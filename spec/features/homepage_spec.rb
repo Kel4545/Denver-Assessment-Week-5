ce555c3955d48ff781562d1bc826fb362eedc4f8
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-07-07 12:36:10 -0600

    Test

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,15 @@
+require "spec helper"
+
+feature "Homepage" do
+  scenario "When logged in a user sees the homepage, can login, see contacts and logout" do
+    visit "/"
+
+    expect (page).to have_content "Contacts"
+    click_button "Login"
+    fill_in("name")
+    fill_in("Password")
+    expect(page).to have_content "Welcome, User"
+    click_button "Logout"
+    expect(page).to have_content "Login"
+  end
+end
